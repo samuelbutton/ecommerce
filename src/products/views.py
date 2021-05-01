b7d7b325f47168a6604af99a4c820fe700df56ac
scbutton95 <scbutton95@gmail.com>
2021-05-01 13:27:15 -0700

    search init complete

--- src/products/views.py
+++ src/products/views.py
@@ -7,13 +7,17 @@ from .models import Product
 
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(
     #         *args, **kwargs)
     #     return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
 
 
 class ProductFeaturedListView(ListView):
