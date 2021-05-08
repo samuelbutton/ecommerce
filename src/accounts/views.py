b4faa673e5c8fe5774841701ecf4c446f3f0fb0d
scbutton95 <scbutton95@gmail.com>
2021-05-07 18:00:29 -0700

    finished user stuff

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import CreateView, FormView
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, get_user_model
 from django.utils.http import is_safe_url
@@ -26,19 +27,19 @@ def guest_register_view(request):
     return redirect("/register/")
 
 
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form": form
-    }
+class LoginView(FormView):
+    form_class = LoginForm
+    success_url = "/"
+    template_name = "accounts/login.html"
 
-    next_ = request.GET.get("next")
-    next_post = request.POST.get("next")
-    redirect_path = next_ or next_post or None
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
+    def form_valid(self, form):
+        request = self.request
+        next_ = request.GET.get("next")
+        next_post = request.POST.get("next")
+        redirect_path = next_ or next_post or None
+        email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             try:
@@ -49,27 +50,38 @@ def login_page(request):
                 return redirect(redirect_path)
             else:
                 return redirect("/")
-        else:
-            print("Error")
+        return super(LoginView, self).form_invalid(form)
 
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+
+    next_ = request.GET.get("next")
+    next_post = request.POST.get("next")
+    redirect_path = next_ or next_post or None
+    if form.is_valid():
+        pass
     return render(request, "accounts/login.html", context)
 
 
 User = get_user_model()
 
 
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {
-        "form": form
-    }
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "accounts/register.html"
+    success_url = "/login/"
 
-    if form.is_valid():
-        print(form.cleaned_data)
-        context['form'] = RegisterForm()
-        username = form.cleaned_data.get('username')
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        new_user = User.objects.create_user(username, email, password)
-        print(new_user)
-    return render(request, "accounts/register.html", context)
+
+# def register_page(request):
+#     form = RegisterForm(request.POST or None)
+#     context = {
+#         "form": form
+#     }
+
+#     if form.is_valid():
+#         form.save()
+#     return render(request, "accounts/register.html", context)
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
-from accounts.views import login_page, register_page, guest_register_view
+from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_detail_api_view
 from .views import home_page, about_page, contact_page
@@ -29,7 +29,7 @@ urlpatterns = [
     re_path(r'^$', home_page, name='home'),
     re_path(r'^about/$', about_page),
     re_path(r'^contact/$', contact_page, name="contact"),
-    re_path(r'^login/$', login_page, name="login"),
+    re_path(r'^login/$', LoginView.as_view(), name="login"),
     re_path(r'^checkout/address/create/$',
             checkout_address_create_view, name="checkout_address_create"),
     re_path(r'^checkout/address/reuse/$',
@@ -38,7 +38,7 @@ urlpatterns = [
     re_path(r'^logout/$', LogoutView.as_view(), name="logout"),
     re_path(r'^api/cart/$', cart_detail_api_view, name="api-cart"),
     re_path(r'^cart/', include(("carts.urls", "carts"), namespace="cart")),
-    re_path(r'^register/$', register_page, name="register"),
+    re_path(r'^register/$', RegisterView.as_view(), name="register"),
     re_path(r'^bootstrap/$',
             TemplateView.as_view(template_name="bootstrap/example.html")),
     re_path(r'^products/', include(("products.urls",
